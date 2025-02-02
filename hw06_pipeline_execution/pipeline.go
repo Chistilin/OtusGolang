ab521ab07303fde7adb745d767649dee46f24cec
Mikhail Chistilin <mihail.chistilin@vitech.team>
2025-02-02 16:12:41 +0300

    hw06_pipeline_execution: Test ДЗ

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,44 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+func ExecutePipeline(in, done In, stages ...Stage) Out {
+	if in == nil {
+		res := make(Bi)
+		close(res)
+		return res
+	}
+	out := in
+	for _, stage := range stages {
+		out = stage(processStage(out, done))
+	}
+
+	return out
+}
+
+func processStage(in, done In) Out {
+	processOut := make(Bi)
+	go func() {
+		defer func() {
+			close(processOut)
+			for range in {
+				continue
+			}
+		}()
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case processOut <- v:
+				}
+			}
+		}
+	}()
+	return processOut
 }
