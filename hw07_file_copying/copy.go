6e5dcfdeed6520f3087ec93e6c9a2a52f5cff6fa
Mikhail Chistilin <mihail.chistilin@vitech.team>
2025-02-03 17:45:31 +0300

    hw07_file_copying: In progress

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -13,9 +13,6 @@ type fileParams struct {
 }
 
 var (
-	ErrCopyFile              = errors.New("error copy file")
-	ErrFileClose             = errors.New("error file closed")
-	ErrFileOpen              = errors.New("error file open")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
@@ -34,7 +31,6 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		//read, err := fromFile.Read(buf[offset:])
 		//offset += read
 		if err == io.EOF {
-			// что если не дочитали ?
 			break
 		}
 		/*		if err != nil {
@@ -43,23 +39,23 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	toFile, err := os.Create(toPath)
 	if err != nil {
-		panic(ErrFileOpen)
+		log.Panicf("error %s file create : %v", toPath, err)
 	}
 
 	defer func(fromFile *os.File, toFile *os.File) {
 		errFrom := fromFile.Close()
 		if errFrom != nil {
-			panic(ErrFileClose)
+			log.Panicf("error %s file close : %v", fromPath, err)
 		}
 		errTo := toFile.Close()
 		if errTo != nil {
-			panic(ErrFileClose)
+			log.Panicf("error %s file close : %v", toPath, err)
 		}
 	}(fromFile, toFile)
 
 	_, err = io.CopyN(fromFile, toFile, limit)
 	if err != nil {
-		panic(ErrCopyFile)
+		log.Panicf("error copy file : %v", fromPath)
 	}
 
 	return nil
