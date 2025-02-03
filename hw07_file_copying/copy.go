5084c6d8b5eee2009e4ba7c0708065aea077e245
Mikhail Chistilin <mihail.chistilin@vitech.team>
2025-02-03 14:31:57 +0300

    hw07_file_copying: In progress

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
+type fileParams struct {
+	offset string
+	limit  int64
+}
+
 var (
+	ErrCopyFile              = errors.New("error copy file")
+	ErrFileClose             = errors.New("error file closed")
+	ErrFileOpen              = errors.New("error file open")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
 
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		panic(ErrFileOpen)
+	}
+
+	defer func(fromFile *os.File, toFile *os.File) {
+		errFrom := fromFile.Close()
+		if errFrom != nil {
+			panic(ErrFileClose)
+		}
+		errTo := toFile.Close()
+		if errTo != nil {
+			panic(ErrFileClose)
+		}
+	}(fromFile, toFile)
+
+	_, err = io.CopyN(fromFile, toFile, limit)
+	if err != nil {
+		panic(ErrCopyFile)
+	}
 	return nil
 }
