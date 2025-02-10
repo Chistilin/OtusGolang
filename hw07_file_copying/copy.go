ece9361a28f6aab62e295e518bf814b22da98b5c
Mikhail Chistilin <mihail.chistilin@vitech.team>
2025-02-10 17:23:47 +0300

    hw07_file_copying: In progress

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
-	"github.com/schollz/progressbar/v3"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
 	"io"
 	"log"
 	"os"
 )
-
-type fileParams struct {
-	offset string
-	limit  int64
-}
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
@@ -19,29 +16,37 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	//Валидируем оба пути
-	validate(fromPath, toPath, offset, limit)
+	//Валидируем перед открытием файла
+	fileInfo, err := validate(fromPath, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	if (fileInfo.Size() - offset) < limit {
+		limit = fileInfo.Size() - offset
+	}
 	//Открываем файл
 	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		log.Panicf("failed %s to read: %v", fromFile, err)
+		return ErrUnsupportedFile
 	}
 	//Проверяем что есть такое смещение
 	if _, err = fromFile.Seek(offset, 0); err != nil {
-		log.Panic(ErrOffsetExceedsFileSize)
+		return ErrOffsetExceedsFileSize
 	}
 	//Создаем файл куда копировать
 	toFile, err := os.Create(toPath)
 	if err != nil {
-		log.Panicf("error %s file create : %v", toPath, err)
+		return fmt.Errorf("error %s file create : %v", toPath, err)
 	}
-	toFileInfo, err := os.Stat(toPath)
+	toFileInfo, err := toFile.Stat()
 	if err != nil {
-		log.Panicf("failed about info To file: %s", toPath)
+		return fmt.Errorf("failed about info To file: %s", toPath)
 	}
 	if toFileInfo.IsDir() {
-		log.Panic(ErrUnsupportedFile)
+		return ErrUnsupportedFile
 	}
+
 	//Закрываем всегда при выходе
 	defer func(fromFile *os.File, toFile *os.File) {
 		errFrom := fromFile.Close()
@@ -55,32 +60,30 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}(fromFile, toFile)
 
 	//Копируем
-	_, err = io.CopyN(toFile, fromFile, offset)
-	if err != nil {
-		log.Panicf("error copy file : %v, %s", fromPath, err)
-	}
-	bar := progressbar.DefaultBytes(
-		offset,
-		"downloading",
-	)
+	buf := bufio.NewReaderSize(fromFile, int(fileInfo.Size()))
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(buf)
+	_, err = io.CopyN(toFile, barReader, limit)
+	bar.Finish()
 	return nil
 }
 
-func validate(fromPath, toPath string, offset, limit int64) {
+func validate(fromPath string, offset, limit int64) (os.FileInfo, error) {
 	if offset < 0 {
-		log.Panic("Offset < 0")
+		return nil, ErrOffsetExceedsFileSize
 	}
 	if limit < 0 {
-		log.Panic("Limit < 0")
+		return nil, fmt.Errorf("Limit < 0")
 	}
 	fromFileInfo, err := os.Stat(fromPath)
 	if err != nil {
-		log.Panicf("failed about info From file: %s", fromPath)
+		return nil, fmt.Errorf("failed about info From file: %s", fromPath)
 	}
 	if fromFileInfo.IsDir() {
-		log.Panicf("File is dir: %s", fromPath)
+		return nil, fmt.Errorf("File is dir: %s", fromPath)
 	}
 	if fromFileInfo.Size() < offset {
-		log.Panicf("File from %s size %s < offset", fromFileInfo.Size(), fromPath)
+		return nil, fmt.Errorf("File from %s size %s < offset", fromFileInfo.Size(), fromPath)
 	}
+	return fromFileInfo, nil
 }
